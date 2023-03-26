f9c796b7dec7cf4bef028046c5d0d712374f04c6
Prince Chauhan <princechauhan546@gmail.com>
2023-03-26 20:41:40 +0530

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
-const homeController = require("../controllers/home_controller")
-
-console.log("router loaded!");
+const homeController = require("../controllers/home_controller");
 
 // Routes for different controllers
-router.get("/",homeController.home);
-router.get("/practice",homeController.practice);
+router.get("/", homeController.home);
+router.get("/practice", homeController.practice);
 router.use("/users", require("./users"));
 router.use("/posts", require("./posts"));
 router.use("/comments", require("./comments"));
 router.use("/likes", require("./likes"));
 
-
-router.use("/api", require("./api"))
+router.use("/api", require("./api"));
 
 module.exports = router;
