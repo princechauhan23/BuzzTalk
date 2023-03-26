9180fbd3b8a4526b05c9e33f7ee359cf105ec65b
Prince Chauhan <princechauhan546@gmail.com>
2023-03-26 19:42:31 +0530

    improve dependencies

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const env = require("./environment");
 
 mongoose.set("strictQuery", false);
-console.log("env",env)
 mongoose.connect(`mongodb+srv://prince:${env.mongoDBpass}@cluster0.nuyf1.mongodb.net/test`);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error connecting to mongoDB"));
--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -4,44 +4,53 @@ const crypto = require("crypto");
 const User = require("../models/user");
 const env = require("./environment");
 
-
 // tell passport to use new startegy for google login
-passport.use(new googleStrategy({
-        clientID:env.google_client_id,
-        clientSecret: env.google_client_secret,
-        callbackURL: env.google_callback_URL,
+passport.use(
+  new googleStrategy(
+    {
+      clientID: env.google_client_id,
+      clientSecret: env.google_client_secret,
+      callbackURL: env.google_callback_URL,
     },
 
-    function(accessToken, refreshToken, profile, done){
-        // find a user 
-        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
-            if (err){console.log("error in google startegy-passport", err); return;}
+    function (accessToken, refreshToken, profile, done) {
+      // find a user
+      User.findOne({ email: profile.emails[0].value }).exec(function (
+        err,
+        user
+      ) {
+        if (err) {
+          console.log("error in google startegy-passport", err);
+          return;
+        }
 
-            console.log(accessToken, refreshToken);
-            console.log(profile);
+        if (user) {
+          // if found, set this user as req.user
+          return done(null, user);
+        } else {
+          // if not found, create the user and set it as req.user
+          User.create(
+            {
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              password: crypto.randomBytes(20).toString("hex"),
+            },
+            function (err, user) {
+              if (err) {
+                console.log(
+                  "error in creating user google startegy-passport",
+                  err
+                );
+                return;
+              }
 
-            if (user){
-                // if found, set this user as req.user
-                return done(null, user);
-            }else{
-                // if not found, create the user and set it as req.user
-                User.create({
-                    name: profile.displayName,
-                    email: profile.emails[0].value,
-                    password: crypto.randomBytes(20).toString("hex")
-                }, function(err, user){
-                    if (err){console.log("error in creating user google startegy-passport", err); return;}
-
-                    return done(null, user);
-                });
+              return done(null, user);
             }
-        });
+          );
+        }
+      });
     }
-
-
-
-));
-
-
+  )
+);
 
 module.exports = passport;
